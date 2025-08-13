904e1a004b8b3c9fabe41a24b2ecfc4a9eb90196
Mukesh Rawat <swissmukeshrawat@gmail.com>
2025-08-13 12:19:20 +0530

    working in Basic JS module

---
+++ 0. Basic JS/1.Variables.js
@@ -0,0 +1,36 @@
+// THis is comment 
+/*Hello Guys
+*/
+var name = "John Doe";
+let name2 = "Mukesh Rawat";
+const name3 = "Rohit Kumar";
+
+let bigNumber = 1234567890123456789184840n;//stores big integer.
+console.log(typeof(bigNumber));
+
+var name4 = `Hello My Name is Mukesh Rawat`;
+
+const obj = {
+  name: "John Doe",
+  name: "Jane Smith",
+  post: {
+    createdAt: "Hello",
+    post: "hsesoefsef",
+  },
+  age: 30,
+  city: "New York",
+};
+let abc;
+console.log(abc);
+let cde = null;
+console.log(cde);
+console.log(typeof obj);
+
+const notAvailable = undefined;
+console.log(notAvailable);
+console.log(obj.post.post);
+console.log(name2);
+
+console.log("Hello Guys:", name, ",", name2, ",", name3);
+
+console.log(name4);
---
+++ 0. Basic JS/2. Operators.js
@@ -0,0 +1,56 @@
+var x = 10;
+console.log(x ** 2); //exponentiation operator
+
+//logical operator (short circuiting): 
+//use case in real world application -> conditional rendering in React, and authentication flows.
+//AND -> if first value is falsy, then dont even try to evaluate second value and return first value.
+//if first value is truthy, then return second value.
+console.log(30 && 45);
+console.log(30 && 0);
+console.log(0 && 45);
+console.log(undefined && 45);//undefined
+console.log("" && 45);
+console.log(null && 45);
+console.log(NaN && 45);
+console.log(0 && 0);//0, it's first argument, not second.
+console.log(NaN && null);//NaN, it's falsy
+console.log("" && -0);
+
+
+//OR ->  if first value(arg) is false, return second value.
+//if first value is truthy, then return first value.
+console.log("testing OR Operator: ");
+console.log(30 || 45);//30
+console.log(30 || 0);//30
+console.log(0 || 45);//45
+console.log(undefined || 45);//45
+console.log("" || 45);//45
+console.log(null || 45);//45
+console.log(NaN || 45);//45
+console.log(0 || 0);//0 
+console.log(NaN || null);//null
+console.log("" || -0);//-0
+
+
+//end of short circuiting.
+
+
+
+//logical operators: strict equality operator(===) vs loose equality (==) AKA abstract equality operator.
+console.log(0 === 0);//true
+console.log(0 === "0");//false
+console.log(0 == "0");//true, because of type coercion(loose equality)
+console.log(0 === false);//false, because types are different
+console.log(0 == false);//true
+console.log(null == undefined);//true
+console.log(null === undefined);//false
+console.log(NaN === NaN);//false
+console.log(NaN == NaN);//false
+
+/*
+Summary:
+-> Loose equality (==) performs type coercion, while strict equality (===) does not.
+*/
+
+//End of logical operators.
+
