ff48657f4f3d6559a65bc1809c2337243eeb6a9e
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-08-27 11:40:22 +0530

    started with prototypes in OOP

--- 2. OOP/26. OOP26.js
+++ 2. OOP/26. OOP26.js
@@ -10,3 +10,5 @@ console.log(extendedNumbers); // [3, 4, 5, 7, 8]
 
 //in array des sequencing matters but the name does not.
 //but in object destructuring, the order doesn't matter, but the name does.
+
+//next folder: Prototypes in OOP
