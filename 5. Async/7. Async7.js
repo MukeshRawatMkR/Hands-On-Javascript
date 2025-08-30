762b1c78f8eb5bf1a0a1c28f3f2a40d65b737941
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-08-30 14:16:21 +0530

    implemented the callbacks

--- 5. Async/7. Async7.js
+++ 5. Async/7. Async7.js
@@ -1,31 +1,45 @@
-//Exercise solution:
+//IQ: Exercise solution:
 
-var file;
-function downloadFile(){
-    console.log("Downloading file...");
-    setTimeout(() => {
-        file = "file content";
-    }, 2000);
-}
 
-function writeFile(file){
+function download(url, callback) {
+    console.log(`downloading from ${url}`);
     setTimeout(() => {
-        console.log("Writing to file...");
-        file = "File content extended";
-        console.log(file);
+        console.log("data downloaded");
+        let data = "sample data";
+        callback?.(data);//?. is optional chaining, which means if callback is not null or undefined, it will be called with data.
     }, 3000);
-}
+};
 
-function upload(file){
+function writeFile(data, fileName, callback) {//filename tell the name of the file to be created in which data will be written.
+    console.log(`writing ${data} to file`);
     setTimeout(() => {
-        console.log("Uploading file...");
-        // console.log(file);
-        console.log("file is uploaded");
-    }, 4000);
+        console.log(`file written on filename ${fileName}`);
+        let status = "success";
+        callback?.(status);
+    }, 2000);
+};
+
+function uploadFile(fileName, url, callback) {
+    console.log(`uploading the file ${fileName} in ${url}`);
+    setTimeout(() => {
+        console.log(`file uploaded to ${url}`);
+        let uploadStatus = "success";
+        callback?.(uploadStatus);
+    }, 3000);
+};
+
+function process() {
+    download("https://example.com", (data) => {
+        writeFile(data, "file.txt", (status) => {
+            uploadFile("file.txt", "https://example1.com/upload", (uploadStatus) => {
+                console.log(`Process completed with status: ${uploadStatus}`);
+            });
+        });
+    });
 }
 
-downloadFile(writeFile(upload()));
+process();
 
-// let process = downloadFile();
 
-//  process();writeFile(upload())
+
+//nextfile: solution using Promises.
